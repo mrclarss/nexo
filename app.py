ef4bfa6e989cf227e7b9bc3aaf0859399d4a2a0e
mrclarss <160539418+mrclarss@users.noreply.github.com>
2026-01-27 22:32:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask, render_template
 import json
 app = Flask(__name__)
 
-
 @app.route('/')
+def index ():
+    return render_template('index.html')
+
+@app.route('/home')
 def base ():
     return render_template('home.html')
 
