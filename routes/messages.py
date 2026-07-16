d3161d95ab06f29c0937dce31d45dafdae4fb112
Maria Clara da Silva Santos <20231041110007@ifrn.local>
2026-07-16 13:30:58 -0300

    atualizando as rotas

--- routes/messages.py
+++ routes/messages.py
@@ -6,13 +6,13 @@ from app.controllers.message_controller import criar_mensagem, listar_mensagens
 messages_bp = Blueprint("messages", __name__)
 
 
-@messages_bp.route("/", methods=["GET"])
+@messages_bp.route("/listar_mensagens", methods=["GET"])
 def get_messages():
     response, status = listar_mensagens()
     return jsonify(response), status
 
 
-@messages_bp.route("/", methods=["POST"])
+@messages_bp.route("/criar_mensagem", methods=["POST"])
 def post_message():
     data = request.get_json()
     response, status = criar_mensagem(data)
--- routes/team.py
+++ routes/team.py
@@ -11,25 +11,25 @@ from app.controllers.team_controller import (
 team_bp = Blueprint("teams", __name__)
 
 
-@team_bp.route("/", methods=["GET"])
+@team_bp.route("/listar_equipe", methods=["GET"])
 def get_team():
     response, status = listar_equipe()
     return jsonify(response), status
 
 
-@team_bp.route("/", methods=["POST"])
+@team_bp.route("/criar_equipe", methods=["POST"])
 def post_team():
     data = request.get_json()
     response, status = criar_equipe(data)
     return jsonify(response), status
 
-@team_bp.route("/<int:id>", methods=["PATCH"])
+@team_bp.route("/<int:id>/atualizar_equipe", methods=["PATCH"])
 def patch_team(id):
     data = request.get_json()
     response, status = atualizar_equipe(id,data)
     return jsonify(response), status
 
-@team_bp.route("/<int:id>", methods=["DELETE"])
+@team_bp.route("/<int:id>/deletar_equipe", methods=["DELETE"])
 def delete_team(id):
     response, status = deletar_equipe(id)
     return jsonify(response), status
