de24e300ddfaa8788043bcba299fc4d219db692d
mrclarss <160539418+mrclarss@users.noreply.github.com>
2026-07-16 13:33:16 -0300

    Change route to '/listar_participants' for participants

--- routes/participantes.py
+++ routes/participantes.py
@@ -6,7 +6,7 @@ from app.controllers.participants_controller import listar_participants
 participants_bp = Blueprint("participants", __name__)
 
 
-@participants_bp.route("/", methods=["GET"])
+@participants_bp.route("/listar_participants", methods=["GET"])
 def get_():
     response, status = listar_participants()
     return jsonify(response), status
